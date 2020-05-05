6fa45f94c06e64e32612ecdde735830317a8a97c
Aaron William <likeclem30@gmail.com>
2020-05-05 18:24:17 +0100

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -4,7 +4,7 @@
 
 # install docker
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-add-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'
 apt-get update
 apt-get install -y docker-engine
 systemctl enable docker
